952f427b1131217971f168d98ded5936136ed804
Typogalaxy <kuobuoo@gmail.com>
2026-02-03 22:43:44 +1100

    trying to fix cors error

--- backend/backend/settings/staging.py
+++ backend/backend/settings/staging.py
@@ -3,8 +3,8 @@ from .base import *
 SECRET_KEY = env.str("SECRET_KEY")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
-
 CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
+CORS_ALLOW_CREDENTIALS = True
 
 DEBUG = False
 
